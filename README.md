be0b54a94d725d83b860ba51ab43430866eb0b58
Rohit Nautiyal <rohitnautiyal1998@gmail.com>
2020-09-30 08:53:13 +0530

    complete the text area of the app

---
+++ README.md
@@ -0,0 +1,1 @@
+The perfect app to help you read any text easily and not worry about the words out of your vocabulary. It helps you build a strong vocabulary and makes you want to read more. It not only helps beginners but just anybody who is looking to add some new words to their vocabulary but are too lazy to look them up in the dictionary cause we've got you covered.
---
+++ script.js
@@ -0,0 +1,128 @@
+const text = '“Stopping by Woods On a Snowy Evening” by Robert Frost\n\nWhose woods these are I think I know.\nHis house is in the village though.\nHe will not see me stopping here\nTo watch his woods fill up with snow.\nMy little horse must think it queer\nTo stop without a farmhouse near\nBetween the woods and frozen lake\nThe darkest evening of the year.\nHe gives his harness bells a shake\nTo ask if there is some mistake.\nThe only other sound’s the sweep\nOf easy wind and downy flake.\nThe woods are lovely, dark and deep,\nBut I have promises to keep,\nAnd miles to go before I sleep,\nAnd miles to go before I sleep.\n';
+const marks = new Set([' ', ',', '.', ':', ';', '"', '\'', '?', '+', '(', ')', '#', '!', '&', '-']);
+let editState = 0;
+const input = document.getElementById("inp");
+const textDiv = document.querySelector(".text-div");
+const editOption = document.querySelector(".edit");
+const doneOption = document.querySelector(".done");
+
+textDiv.textContent = text;
+input.value = text;
+// input.style.minHeight = `${input.scrollHeight}px`;
+// input.value = text + text + text;
+
+////////////////////////////////////////////
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+document.onmouseup = document.onkeyup = function () {
+    let text = getSelectionText();
+    if(text){
+        processSelection(text);
+    }
+};
+////////////////////////////////////////////
+
+function cleanSelection(str) {
+    if (marks.has(str[0])) {
+        str = str.slice(1, );
+    }
+    let l = str.length - 1;
+    if (marks.has(str[l])) {
+        str = str.slice(0, l);
+    }
+    return str;
+}
+
+function processSelection(selection) {
+    selection = cleanSelection(selection);
+    if (selection.length > 0 && selection.split(' ').length === 1) {
+        searchWord(selection);
+    }
+}
+/////////////////////////////////////////////
+
+// input.addEventListener('select', (e) => {
+//     if (state) {
+//         let selection = cleanSelection(e.target.value.substring(e.target.selectionStart, e.target.selectionEnd));
+
+//         if (selection.length > 0 && selection.split(' ').length === 1) {
+//             searchWord(selection);
+//         }
+//     }
+// });
+
+editOption.addEventListener('click', (e) => {
+    if(!editState){
+        // input.value = textDiv.textContent;
+        textDiv.classList.toggle("hide");
+        input.classList.toggle("hide");
+        input.select();
+        editOption.classList.toggle("hide");
+        doneOption.classList.toggle("hide");
+        editState = 1;
+    }
+    // input.setAttribute("readonly", "readonly");
+});
+doneOption.addEventListener('click', (e) => {
+    if(editState){
+        textDiv.textContent = input.value;
+        textDiv.classList.toggle("hide");
+        input.classList.toggle("hide");
+        input.select();
+        editOption.classList.toggle("hide");
+        doneOption.classList.toggle("hide");
+        editState = 0;
+    }
+});
+
+async function searchWord(word) {
+    console.clear();
+    console.log(word);
+    let url = `https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=api-key`;
+    let response = await axios.get(url);
+    let count = 1;
+    console.log(response);
+    response.data.forEach(def => {
+        if (def.def) {
+            console.log('Definition:', count);
+            def.def[0].sseq.forEach(sseq => {
+                sseq.forEach(b => {
+                    if (b[1].dt) {
+                        b[1].dt.forEach(c => {
+                            if (c[0] === "text") {
+                                console.log("Meaning: ", c[1])
+                            };
+                            if (c[0] === "vis") {
+                                console.log("Example: ", c[1][0].t)
+                            };
+                        })
+                    }
+                    // console.log(data[0][1]);
+                })
+            })
+            count++;
+        } else {
+            // searchWord(def.meta.stems[2]);
+        }
+        // console.log(data[0][1], data[1][1][0].t.replace('{wi}', '#'));
+    });
+}
+
+// input.addEventListener('change', (e)=>{
+//     console.log('change');
+//     if (!e.target.value.startsWith('\n')){
+//         e.target.value = '\n' + e.target.value;
+//     }
+//     if(!e.target.value.endsWith('\n')){
+//         e.target.value = e.target.value + '\n';
+//     }  
+// });
